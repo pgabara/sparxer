94f4dcae259b86315bb6ca9ad17f392cb3e527d0
Piotr Gabara <piotr.gabara@hotmail.com>
2017-11-16 22:23:22 +0100

    Add new test cases

--- adapter/src/test/scala/com/github/bhop/sparxer/adapter/SparkAppHandleToObservableTest.scala
+++ adapter/src/test/scala/com/github/bhop/sparxer/adapter/SparkAppHandleToObservableTest.scala
@@ -1,5 +1,6 @@
 package com.github.bhop.sparxer.adapter
 
+import com.github.bhop.sparxer.adapter.domain.JobState
 import monix.execution.Scheduler
 import org.apache.spark.launcher.SparkAppHandle
 import org.scalatest.concurrent.ScalaFutures
@@ -17,10 +18,18 @@ class SparkAppHandleToObservableTest extends WordSpec with Matchers with ScalaFu
       val states = converter.toObservable.map(_.state).foldLeftL(List.empty[String]) { (acc, state) => state :: acc }
       states.runAsync.futureValue should be(List("FINISHED", "RUNNING", "SUBMITTED", "CONNECTED"))
     }
+
+    "handle SparkAppHandle internal exception" in {
+      val handler = new StubSparkAppHandle(fail = true)
+      val converter = new implicits.SparkAppHandleToObservable(handler)
+      val states = converter.toObservable.onErrorHandle(_ => JobState(None, "Error!"))
+      states.runAsyncGetFirst.futureValue should be(Some(JobState(None, "Error!")))
+    }
   }
 }
 
-class StubSparkAppHandle(state: SparkAppHandle.State = SparkAppHandle.State.UNKNOWN) extends SparkAppHandle {
+class StubSparkAppHandle(state: SparkAppHandle.State = SparkAppHandle.State.UNKNOWN,
+                         fail: Boolean = false) extends SparkAppHandle {
 
   override def disconnect(): Unit = ()
 
@@ -34,6 +43,7 @@ class StubSparkAppHandle(state: SparkAppHandle.State = SparkAppHandle.State.UNKN
 
   override def addListener(l: SparkAppHandle.Listener): Unit = {
     import SparkAppHandle.State._
+    if (fail) throw new IllegalStateException("Something is wrong!")
     val states = List(CONNECTED, SUBMITTED, RUNNING, FINISHED)
     states.foreach(s => l.stateChanged(new StubSparkAppHandle(s)))
   }
